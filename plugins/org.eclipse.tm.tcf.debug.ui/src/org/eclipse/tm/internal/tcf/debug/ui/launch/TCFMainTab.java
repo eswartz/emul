2dee78a67b8f74243b87621b1943edcf8fe3510a
eutarass <eutarass@6a79697e-3843-0410-8446-a9668620458d>
2008-03-12 18:46:23 +0000

    Bug 222363: Initial implementation of value-add

--- plugins/org.eclipse.tm.tcf.debug.ui/src/org/eclipse/tm/internal/tcf/debug/ui/launch/TCFMainTab.java
+++ plugins/org.eclipse.tm.tcf.debug.ui/src/org/eclipse/tm/internal/tcf/debug/ui/launch/TCFMainTab.java
@@ -124,7 +124,6 @@ public class TCFMainTab extends AbstractLaunchConfigurationTab {
                             updateItems();
                         }
                     }
-                    assert false;
                 }
             });
         }
