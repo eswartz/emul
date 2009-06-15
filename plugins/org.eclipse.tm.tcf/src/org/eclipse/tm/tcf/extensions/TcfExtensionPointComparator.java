cc9e587afd2aadb640755dd7b76ec4ddc98196d2
eutarass <eutarass@6a79697e-3843-0410-8446-a9668620458d>
2009-06-15 19:59:15 +0000

    Changed line separator to LF to be consistent with the rest of the code.

--- plugins/org.eclipse.tm.tcf/src/org/eclipse/tm/tcf/extensions/TcfExtensionPointComparator.java
+++ plugins/org.eclipse.tm.tcf/src/org/eclipse/tm/tcf/extensions/TcfExtensionPointComparator.java
@@ -18,7 +18,7 @@ import org.eclipse.core.runtime.IExtension;
  * TCF extension point comparator. Used to asure that extension are
  * always read in the same order.
  * <p>
- * The order of the extensions is definded as following:<br>
+ * The order of the extensions is defined as following:<br>
  * <ul><li>Extensions contributed by the TCF core plug-ins (<code>org.eclipse.tm.tcf.*</code>)
  *         in ascending alphabetic order and</li>
  *     <li>Extensions contributed by any other plug-in in ascending alphabetic order.</li>
@@ -27,42 +27,42 @@ import org.eclipse.core.runtime.IExtension;
  * </ul>
  */
 public class TcfExtensionPointComparator implements Comparator<IExtension> {
-	private final static String TCF_PLUGIN_PATTERN = "org.eclipse.tm.tcf.*"; //$NON-NLS-1$
+    private final static String TCF_PLUGIN_PATTERN = "org.eclipse.tm.tcf.*"; //$NON-NLS-1$
 
-	/* (non-Javadoc)
-	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
-	 */
-	public int compare(IExtension o1, IExtension o2) {
-		// We ignore any comparisation with null and
-		if (o1 == null || o2 == null) return 0;
-		// Check if it is the exact same element
-		if (o1 == o2) return 0;
+    /* (non-Javadoc)
+     * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+     */
+    public int compare(IExtension o1, IExtension o2) {
+        // We ignore any comparisation with null and
+        if (o1 == null || o2 == null) return 0;
+        // Check if it is the exact same element
+        if (o1 == o2) return 0;
 
-		// The extensions are compared by the unique id of the contributing plugin first
-		String contributor1 = o1.getContributor().getName();
-		String contributor2 = o2.getContributor().getName();
+        // The extensions are compared by the unique id of the contributing plugin first
+        String contributor1 = o1.getContributor().getName();
+        String contributor2 = o2.getContributor().getName();
 
-		// Contributions from TCF core plugins comes before 3rdParty Plugins
-		if (contributor1.startsWith(TCF_PLUGIN_PATTERN) && !contributor2.startsWith(TCF_PLUGIN_PATTERN))
-			return -1;
-		if (!contributor1.startsWith(TCF_PLUGIN_PATTERN) && contributor2.startsWith(TCF_PLUGIN_PATTERN))
-			return 1;
-		if (contributor1.startsWith(TCF_PLUGIN_PATTERN) && contributor2.startsWith(TCF_PLUGIN_PATTERN)) {
-			int value = contributor1.compareTo(contributor2);
-			// Within the same plugins, the extension are sorted by thier unique id (if available)
-			if (value == 0 && o1.getUniqueIdentifier() != null && o2.getUniqueIdentifier() != null)
-				return o1.getUniqueIdentifier().compareTo(o2.getUniqueIdentifier());
-			// Otherwise, just return the comparisation result from the contributors
-			return value;
-		}
+        // Contributions from TCF core plugins comes before 3rdParty Plugins
+        if (contributor1.startsWith(TCF_PLUGIN_PATTERN) && !contributor2.startsWith(TCF_PLUGIN_PATTERN))
+            return -1;
+        if (!contributor1.startsWith(TCF_PLUGIN_PATTERN) && contributor2.startsWith(TCF_PLUGIN_PATTERN))
+            return 1;
+        if (contributor1.startsWith(TCF_PLUGIN_PATTERN) && contributor2.startsWith(TCF_PLUGIN_PATTERN)) {
+            int value = contributor1.compareTo(contributor2);
+            // Within the same plugins, the extension are sorted by thier unique id (if available)
+            if (value == 0 && o1.getUniqueIdentifier() != null && o2.getUniqueIdentifier() != null)
+                return o1.getUniqueIdentifier().compareTo(o2.getUniqueIdentifier());
+            // Otherwise, just return the comparisation result from the contributors
+            return value;
+        }
 
-		// Contributions from all other plugins are sorted alphabetical
-		int value = contributor1.compareTo(contributor2);
-		// Within the same plugins, the extension are sorted by thier unique id (if available)
-		if (value == 0 && o1.getUniqueIdentifier() != null && o2.getUniqueIdentifier() != null)
-			return o1.getUniqueIdentifier().compareTo(o2.getUniqueIdentifier());
-		// Otherwise, just return the comparisation result from the contributors
-		return value;
-	}
+        // Contributions from all other plugins are sorted alphabetical
+        int value = contributor1.compareTo(contributor2);
+        // Within the same plugins, the extension are sorted by thier unique id (if available)
+        if (value == 0 && o1.getUniqueIdentifier() != null && o2.getUniqueIdentifier() != null)
+            return o1.getUniqueIdentifier().compareTo(o2.getUniqueIdentifier());
+        // Otherwise, just return the comparisation result from the contributors
+        return value;
+    }
 
 }
