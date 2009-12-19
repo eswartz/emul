fa99162500f7b6987fd76c0e695802127334ce92
eutarass <eutarass@6a79697e-3843-0410-8446-a9668620458d>
2009-12-19 00:41:43 +0000

    TCF Agent: more support for developing value-add TCF servers, including: 1. remote data cache abstraction. 2. support for storing and re-sending TCF error reports: struct ErrorReport. 3. new service: Memory Map. 4. byte array output stream implementation.

---
+++ plugins/org.eclipse.tm.tcf.core/src/org/eclipse/tm/internal/tcf/services/remote/MemoryMapProxy.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Wind River Systems - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tm.internal.tcf.services.remote;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.tm.tcf.core.Command;
+import org.eclipse.tm.tcf.protocol.IChannel;
+import org.eclipse.tm.tcf.protocol.IToken;
+import org.eclipse.tm.tcf.protocol.JSON;
+import org.eclipse.tm.tcf.services.IMemoryMap;
+
+public class MemoryMapProxy implements IMemoryMap {
+
+    private final IChannel channel;
+    private final Map<MemoryMapListener,IChannel.IEventListener> listeners =
+        new HashMap<MemoryMapListener,IChannel.IEventListener>();
+
+    private class Region implements MemoryRegion {
+
+        final Map<String,Object> props;
+
+        Region(Map<String,Object> props) {
+            this.props = props;
+        }
+
+        public Number getAddress() {
+            return (Number)props.get(PROP_ADDRESS);
+        }
+
+        public Number getOffset() {
+            return (Number)props.get(PROP_OFFSET);
+        }
+
+        public Number getSize() {
+            return (Number)props.get(PROP_SIZE);
+        }
+
+        public String getFileName() {
+            return (String)props.get(PROP_FILE_NAME);
+        }
+
+        public int getFlags() {
+            Number n = (Number)props.get(PROP_FLAGS);
+            if (n != null) return n.intValue();
+            return 0;
+        }
+
+        public Map<String,Object> getProperties() {
+            return props;
+        }
+    }
+
+    public MemoryMapProxy(IChannel channel) {
+        this.channel = channel;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public IToken get(String id, final DoneGet done) {
+        return new Command(channel, this, "get", new Object[]{ id }) {
+            @Override
+            public void done(Exception error, Object[] args) {
+                MemoryRegion[] map = null;
+                if (error == null) {
+                    assert args.length == 2;
+                    error = toError(args[0]);
+                    map = toMemoryMap(args[1]);
+                }
+                done.doneGet(token, error, map);
+            }
+        }.token;
+    }
+
+    @SuppressWarnings("unchecked")
+    private MemoryRegion[] toMemoryMap(Object o) {
+        if (o == null) return null;
+        int i = 0;
+        Collection<Object> c = (Collection<Object>)o;
+        MemoryRegion[] map = new MemoryRegion[c.size()];
+        for (Object x : c) map[i++] = toMemoryRegion(x);
+        return map;
+    }
+
+    @SuppressWarnings("unchecked")
+    private MemoryRegion toMemoryRegion(Object o) {
+        if (o == null) return null;
+        return new Region((Map<String,Object>)o);
+    }
+
+    public void addListener(final MemoryMapListener listener) {
+        IChannel.IEventListener l = new IChannel.IEventListener() {
+
+            public void event(String name, byte[] data) {
+                try {
+                    Object[] args = JSON.parseSequence(data);
+                    if (name.equals("changed")) {
+                        assert args.length == 1;
+                        listener.changed((String)args[0]);
+                    }
+                    else {
+                        throw new IOException("Memory Map service: unknown event: " + name);
+                    }
+                }
+                catch (Throwable x) {
+                    channel.terminate(x);
+                }
+            }
+        };
+        channel.addEventListener(this, l);
+        listeners.put(listener, l);
+    }
+
+    public void removeListener(MemoryMapListener listener) {
+        IChannel.IEventListener l = listeners.remove(listener);
+        if (l != null) channel.removeEventListener(this, l);
+    }
+}
---
+++ plugins/org.eclipse.tm.tcf.core/src/org/eclipse/tm/tcf/services/IMemoryMap.java
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Wind River Systems - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tm.tcf.services;
+
+import java.util.Map;
+
+import org.eclipse.tm.tcf.protocol.IService;
+import org.eclipse.tm.tcf.protocol.IToken;
+
+/**
+ * IMemoryMap service provides information about executable modules (files) mapped (loaded) into target memory.
+ */
+public interface IMemoryMap extends IService {
+
+    static final String NAME = "MemoryMap";
+
+    /**
+     * Memory region property names.
+     */
+    static final String
+        /** Number, region address in memory */
+        PROP_ADDRESS = "Addr",
+
+        /** Number, region size */
+        PROP_SIZE = "Size",
+
+        /** Number, region offset in the file */
+        PROP_OFFSET = "Offs",
+
+        /** Number, region flags, see FLAG_* */
+        PROP_FLAGS = "Flags",
+
+        /** String, name of the file */
+        PROP_FILE_NAME = "FileName";
+
+    /**
+     * Memory region flags.
+     */
+    static final int
+        /** Read access is allowed */
+        FLAG_READ = 1,
+
+        /** Write access is allowed */
+        FLAG_WRITE = 2,
+
+        /** Instruction fetch access is allowed */
+        FLAG_EXECUTE = 4;
+
+    /**
+     * Memory region interface.
+     */
+    interface MemoryRegion {
+
+        /**
+         * Get region properties. See PROP_* definitions for property names.
+         * Properties are read only, clients should not try to modify them.
+         * @return Map of region properties.
+         */
+        Map<String,Object> getProperties();
+
+        /**
+         * Get memory region address.
+         * @return region address.
+         */
+        Number getAddress();
+
+        /**
+         * Get memory region size.
+         * @return region size.
+         */
+        Number getSize();
+
+        /**
+         * Get memory region file offset.
+         * @return file offset.
+         */
+        Number getOffset();
+
+        /**
+         * Get memory region flags.
+         * @return region flags.
+         */
+        int getFlags();
+
+        /**
+         * Get memory region file name.
+         * @return file name.
+         */
+        String getFileName();
+    }
+
+    /**
+     * Retrieve memory map for given context ID.
+     *
+     * @param id – context ID.
+     * @param done - call back interface called when operation is completed.
+     * @return - pending command handle.
+     */
+    IToken get(String id, DoneGet done);
+
+    /**
+     * Client call back interface for get().
+     */
+    interface DoneGet {
+        /**
+         * Called when memory map data retrieval is done.
+         * @param error – error description if operation failed, null if succeeded.
+         * @param map – memory map data.
+         */
+        void doneGet(IToken token, Exception error, MemoryRegion[] map);
+    }
+
+    /**
+     * Add memory map event listener.
+     * @param listener - memory map event listener to add.
+     */
+    void addListener(MemoryMapListener listener);
+
+    /**
+     * Remove memory map event listener.
+     * @param listener - memory map event listener to remove.
+     */
+    void removeListener(MemoryMapListener listener);
+
+    /**
+     * Service events listener interface.
+     */
+    interface MemoryMapListener {
+
+        /**
+         * Called when context memory map changes.
+         * @param context_id - context ID.
+         */
+        void changed(String context_id);
+    }
+}
