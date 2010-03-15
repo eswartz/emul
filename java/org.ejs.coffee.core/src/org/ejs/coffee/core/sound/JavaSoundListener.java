c55284858eacd482606933dd57311175a2efeed3
Ed Swartz <ejs@juncture>
2010-03-14 21:40:05 -0500

    Warnings

--- java/org.ejs.coffee.core/src/org/ejs/coffee/core/sound/JavaSoundListener.java
+++ java/org.ejs.coffee.core/src/org/ejs/coffee/core/sound/JavaSoundListener.java
@@ -5,13 +5,10 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Control;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.Mixer;
 import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.Control.Type;
 
 
 
