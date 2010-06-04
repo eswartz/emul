8217215f8715949a124995632605640710f8a4e6
eutarass <eutarass@6a79697e-3843-0410-8446-a9668620458d>
2010-06-04 03:20:06 +0000

    TCF version changed to 0.3.0

--- plugins/org.eclipse.tm.tcf.debug.ui/src/org/eclipse/tm/internal/tcf/debug/ui/launch/setup/WizardLogPage.java
+++ plugins/org.eclipse.tm.tcf.debug.ui/src/org/eclipse/tm/internal/tcf/debug/ui/launch/setup/WizardLogPage.java
@@ -136,7 +136,7 @@ class WizardLogPage extends WizardPage implements Runnable {
             String os = waitPrompt().replace('\n', ' ').trim();
             send("uname -m", true);
             String machine = waitPrompt().replace('\n', ' ').trim();
-            String version = "0.0.1";
+            String version = "0.3.0";
             Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
 
             URL url = FileLocator.find(bundle, new Path("agent/get-os-tag"), null);
