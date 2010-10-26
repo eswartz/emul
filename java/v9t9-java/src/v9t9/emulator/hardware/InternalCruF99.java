f4e9e949a2cfa67f0fbf8d2eb8a9ff768d7c0015
Ed Swartz <ejs@juncture>
2010-10-25 21:06:36 -0500

    Add missing files

---
+++ java/v9t9-java/src/v9t9/emulator/hardware/InternalCruF99.java
@@ -0,0 +1,87 @@
+/*
+ * (c) Ed Swartz, 2010
+ *
+ */
+package v9t9.emulator.hardware;
+
+import v9t9.emulator.common.Machine;
+import v9t9.keyboard.KeyboardState;
+
+/**
+ * CRU handlers for the F99 machine.
+ * @author ejs
+ */
+public class InternalCruF99 extends BaseCruAccess {
+
+	public final static int CRU_BASE = 0x80;
+	
+	/** enabled interrupts (r/w) */
+	public final static int INTS = CRU_BASE + 0;
+	/** pending interrupts (r) */
+	public final static int INTSP = CRU_BASE + 1;
+	/** keyboard column (w) or bits (r) */
+	public final static int KBD = CRU_BASE + 2;
+	/** keyboard alpha */
+	public final static int KBDA = CRU_BASE + 3;
+	
+	/**
+	 * @param machine
+	 * @param keyboardState
+	 */
+	public InternalCruF99(Machine machine, KeyboardState keyboardState) {
+		super(machine, keyboardState, 8);
+	}
+	
+	public void handleWrite(int addr, byte val) {
+		switch (addr) {
+		case INTS:
+			enabledIntMask = val;
+			break;
+			
+		case INTSP:
+			for (int i = 0; i < 8; i++)
+				if ((currentints & (1 << i)) != 0)
+					acknowledgeInterrupt(i);
+			break;
+			
+		case KBD:
+			crukeyboardcol = val & 0x7;
+			
+			keyboardState.resetProbe();
+			if (keyboardState.isPasting())
+				keyboardState.pushQueuedKey();
+			
+			break;
+			
+		case KBDA:
+			alphaLockMask = (val & 0x80) != 0;
+			break;
+		}
+	}
+	
+	public byte handleRead(int addr) {
+		switch (addr) {
+		case INTS:
+			return (byte) (enabledIntMask);
+		case INTSP:
+			return (byte) (currentints & enabledIntMask);
+			
+		case KBD: {
+			int alphamask = 0;
+			
+			if (!alphaLockMask)
+				alphamask = keyboardState.getAlpha() ? 0x10 : 0x0;
+			
+			int col = keyboardState.getKeyboardRow(crukeyboardcol);
+			return (byte) (col | alphamask);
+		}
+		
+		case KBDA:
+			keyboardState.setProbe();
+			
+			return (byte) (keyboardState.getAlpha() ? 1 : 0);
+		}
+		
+		return 0;
+	}
+}
