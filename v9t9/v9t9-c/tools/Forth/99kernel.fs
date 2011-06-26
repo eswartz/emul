7e55953fd10f141f5ad4b0e90cf0214fa04bb7ca
Ed Swartz <ejs@juncture>
2011-06-26 18:26:15 -0500

    Meaningless hacking on broken FORTH

--- v9t9/v9t9-c/tools/Forth/99kernel.fs
+++ v9t9/v9t9-c/tools/Forth/99kernel.fs
@@ -17,7 +17,7 @@ include user.fs
 
 order
 
-(up0) tup .s !   \ even if we don't have USER variables, this will be used for other stuff we want in RAM
+(up0) tup h ! lock   \ even if we don't have USER variables, this will be used for other stuff we want in RAM
 
 order
 
