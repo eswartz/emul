ad54d2cec99a939280e9c8521b82bc48abe88ba9
ustieber <ustieber@6a79697e-3843-0410-8446-a9668620458d>
2011-08-04 13:00:01 +0000

    Target Explorer: Added convenience method to create an empty space between controls

--- target_explorer/plugins/org.eclipse.tm.te.ui/src/org/eclipse/tm/te/ui/wizards/pages/AbstractWizardPage.java
+++ target_explorer/plugins/org.eclipse.tm.te.ui/src/org/eclipse/tm/te/ui/wizards/pages/AbstractWizardPage.java
@@ -9,9 +9,16 @@
  *******************************************************************************/
 package org.eclipse.tm.te.ui.wizards.pages;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.tm.te.ui.utils.SWTControlUtil;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 
 /**
  * An abstract common wizard page implementation.
@@ -73,6 +80,30 @@ public abstract class AbstractWizardPage extends WizardPage {
 		if (contextHelpId != null) {
 			PlatformUI.getWorkbench().getHelpSystem().displayHelp(contextHelpId);
 		}
+	}
+
+	/**
+	 * Convenience method to create a "invisible" label for creating an
+	 * empty space between controls.
+	 *
+	 * @param parent The parent composite. Must not be <code>null</code>.
+	 * @param span The horizontal span.
+	 * @param toolkit The form toolkit or <code>null</code>.
+	 *
+	 * @return
+	 */
+	protected Label createEmptySpace(Composite parent, int span, FormToolkit toolkit) {
+		Assert.isNotNull(parent);
+
+		Label emptySpace = toolkit != null ? toolkit.createLabel(parent, null) : new Label(parent, SWT.NONE);
+
+		GridData layoutData = new GridData(SWT.BEGINNING, SWT.BEGINNING, false, false);
+		layoutData.horizontalSpan = span;
+		layoutData.widthHint = 0; layoutData.heightHint = SWTControlUtil.convertHeightInCharsToPixels(emptySpace, 1);
+
+		emptySpace.setLayoutData(layoutData);
+
+		return emptySpace;
 	}
 
 	/**
