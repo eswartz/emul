0a80b489e4bd16c0e1bb3b1e83ae35d2f0f4e92e
ustieber <ustieber@6a79697e-3843-0410-8446-a9668620458d>
2011-08-07 07:52:27 +0000

    Target Explorer: Added persistence delegate extension point

---
+++ target_explorer/plugins/org.eclipse.tm.te.core.persistence/src/org/eclipse/tm/te/core/persistence/interfaces/IPersistenceDelegate.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Wind River Systems, Inc. and others. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Wind River Systems - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tm.te.core.persistence.interfaces;
+
+import org.eclipse.tm.te.core.interfaces.IExecutableExtension;
+
+/**
+ * Target Explorer: Persistence delegate API declaration
+ */
+public interface IPersistenceDelegate extends IExecutableExtension {
+
+}
