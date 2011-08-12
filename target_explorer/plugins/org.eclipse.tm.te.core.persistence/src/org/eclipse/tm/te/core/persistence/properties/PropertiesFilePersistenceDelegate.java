78dd8d4686e04be7eb825ad7e704ee9409e45dfd
ustieber <ustieber@6a79697e-3843-0410-8446-a9668620458d>
2011-08-12 06:26:35 +0000

    Target Explorer: Implemented read of grouped properties file

--- target_explorer/plugins/org.eclipse.tm.te.core.persistence/src/org/eclipse/tm/te/core/persistence/properties/PropertiesFilePersistenceDelegate.java
+++ target_explorer/plugins/org.eclipse.tm.te.core.persistence/src/org/eclipse/tm/te/core/persistence/properties/PropertiesFilePersistenceDelegate.java
@@ -9,19 +9,26 @@
  *******************************************************************************/
 package org.eclipse.tm.te.core.persistence.properties;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.tm.te.core.interfaces.nodes.IPropertiesContainer;
+import org.eclipse.tm.te.core.nodes.PropertiesContainer;
 import org.eclipse.tm.te.core.persistence.AbstractPersistenceDelegate;
 
 /**
@@ -62,14 +69,29 @@ public class PropertiesFilePersistenceDelegate extends AbstractPersistenceDelega
 	 * @see org.eclipse.tm.te.core.persistence.interfaces.IPersistenceDelegate#delete(org.eclipse.core.runtime.IPath)
 	 */
 	public boolean delete(IPath path) throws IOException {
-		return false;
+		Assert.isNotNull(path);
+		return path.toFile().delete();
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.tm.te.core.persistence.interfaces.IPersistenceDelegate#read(org.eclipse.core.runtime.IPath)
 	 */
 	public IPropertiesContainer read(IPath path) throws IOException {
-		return null;
+		Assert.isNotNull(path);
+
+		IPropertiesContainer data = new PropertiesContainer();
+		Map<String, Object> coreSection = new HashMap<String, Object>();
+
+		// Create the reader object
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path.toFile()), "UTF-8")); //$NON-NLS-1$
+		try {
+			read(reader, coreSection);
+		} finally {
+			reader.close();
+		}
+		data.setProperties(coreSection);
+
+		return !data.getProperties().isEmpty() ? data : null;
 	}
 
 	/**
@@ -131,6 +153,79 @@ public class PropertiesFilePersistenceDelegate extends AbstractPersistenceDelega
 				String newSection = "core".equals(section) ? key.trim() : section + "." + key.trim(); //$NON-NLS-1$ //$NON-NLS-2$
 				// Write it
 				writeMap(writer, newSection, (Map<String, Object>)data.get(key));
+			}
+		}
+	}
+
+	private static Pattern SECTION = Pattern.compile("\\s*\\[([^\\]]+)\\]\\s*"); //$NON-NLS-1$
+	private static Pattern PROPERTY = Pattern.compile("\\s*(.+\\s*=\\s*.+)"); //$NON-NLS-1$
+
+	/**
+	 * Read the data.
+	 *
+	 * @param reader The reader. Must not be <code>null</code>.
+	 * @param data The data. Must not be <code>null</code>.
+	 */
+	protected void read(BufferedReader reader, Map<String, Object> data) throws IOException {
+		Assert.isNotNull(reader);
+		Assert.isNotNull(data);
+
+		// The sections by name for easier access.
+		// The "core" section is the incoming data object
+		Map<String, Map<String, Object>> sections = new HashMap<String, Map<String, Object>>();
+		sections.put("core", data); //$NON-NLS-1$
+
+		String currentSection = "core"; //$NON-NLS-1$
+		String line = reader.readLine();
+		while (line != null) {
+			Matcher matcher = SECTION.matcher(line);
+			if (matcher.matches()) {
+				currentSection = matcher.group(1).toLowerCase();
+				if (sections.get(currentSection) == null) {
+					sections.put(currentSection, new HashMap<String, Object>());
+				}
+			} else {
+				matcher = PROPERTY.matcher(line);
+				if (matcher.matches()) {
+					String property = matcher.group(1);
+					String[] pieces = property.split("=", 2); //$NON-NLS-1$
+					Map<String, Object> section = sections.get(currentSection);
+					section.put(pieces[0].trim(), pieces[1].trim());
+				}
+			}
+
+			line = reader.readLine();
+		}
+
+		// Recreate the sections hierarchy
+		for (String sectionName : sections.keySet()) {
+			if ("core".equals(sectionName)) continue; //$NON-NLS-1$
+			Map<String, Object> section = sections.get(sectionName);
+			if (sectionName.contains(".")) { //$NON-NLS-1$
+				// Split the section name and recreate the missing hierarchy
+				String[] pieces = sectionName.split("\\."); //$NON-NLS-1$
+				Map<String, Object> parentSection = data;
+				for (String subSectionName : pieces) {
+					if ("core".equals(subSectionName)) continue; //$NON-NLS-1$
+
+					if (sectionName.endsWith(subSectionName)) {
+						parentSection.put(subSectionName, section);
+					} else {
+						@SuppressWarnings("unchecked")
+						Map<String, Object> subSection = (Map<String, Object>)parentSection.get(subSectionName);
+						if (subSection == null) {
+							subSection = new HashMap<String, Object>();
+							parentSection.put(subSectionName, subSection);
+						}
+						parentSection = subSection;
+					}
+				}
+			} else {
+				// Place it into the root object, but check if it may exist
+				@SuppressWarnings("unchecked")
+				Map<String, Object> oldSection = (Map<String, Object>)data.get(sectionName);
+				if (oldSection != null) oldSection.putAll(section);
+				else data.put(sectionName, section);
 			}
 		}
 	}
