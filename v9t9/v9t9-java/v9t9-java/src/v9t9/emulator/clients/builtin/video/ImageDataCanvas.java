7975e702630e583100388b81c8aa751fad696417
Ed Swartz <ejs@juncture>
2011-09-13 10:40:35 -0500

    Improve dithering of imported images in 16-color paletted mode.

--- v9t9/v9t9-java/v9t9-java/src/v9t9/emulator/clients/builtin/video/ImageDataCanvas.java
+++ v9t9/v9t9-java/v9t9-java/src/v9t9/emulator/clients/builtin/video/ImageDataCanvas.java
@@ -174,9 +174,18 @@ public abstract class ImageDataCanvas extends BitmapVdpCanvas {
 		if (format == null || format == Format.TEXT || format == Format.COLOR16_8x8)
 			return;
 
+		boolean limitDither = false;
+
 		int ncols;
-		if (format == Format.COLOR16_1x1 || format == Format.COLOR16_8x1 || format == Format.COLOR16_4x4) {
+		if (format == Format.COLOR16_8x1 || format == Format.COLOR16_4x4) {
 			ncols = 16;
+			
+			limitDither = true;
+		}
+		else if (format == Format.COLOR16_1x1) {
+			ncols = 16;
+			
+			optimizePalette(img);
 		}
 		else if (format == Format.COLOR4_1x1) {
 			ncols = 4;
@@ -203,7 +212,6 @@ public abstract class ImageDataCanvas extends BitmapVdpCanvas {
 		
 		updatePaletteMapping();
 
-		boolean limitDither = format == Format.COLOR16_8x1 || format == Format.COLOR16_4x4;
 		for (int y = 0; y < img.getHeight(); y++) {
 			for (int x = 0; x < img.getWidth(); x++) {
 				ditherize(img, x, y, ncols, limitDither);
@@ -212,64 +220,7 @@ public abstract class ImageDataCanvas extends BitmapVdpCanvas {
 		
 		if (format == Format.COLOR16_8x1) {
 			
-			Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-			
-			for (int y = 0; y < img.getHeight(); y++) {
-				for (int x = 0; x < img.getWidth(); x += 8) {
-
-					histogram.clear();
-					
-					int maxx = Math.min(x + 8, img.getWidth());
-					
-					for (int xd = x; xd < maxx; xd++) {
-						int pixel = img.getRGB(xd, y);
-						
-						Integer cnt = histogram.get(pixel);
-						if (cnt == null)
-							cnt = 1;
-						else
-							cnt++;
-						histogram.put(pixel, cnt);
-					}
-					
-					// get prominent colors
-					List<Pair<Integer, Integer>> sorted = new ArrayList<Pair<Integer,Integer>>();
-					for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
-						sorted.add(new Pair<Integer, Integer>(entry.getKey(), entry.getValue()));
-					}
-					Collections.sort(sorted, new Comparator<Pair<Integer, Integer>>() {
-
-						@Override
-						public int compare(Pair<Integer, Integer> o1,
-								Pair<Integer, Integer> o2) {
-							return o2.second - o1.second;
-						}
-						
-					});
-
-					int fpixel, bpixel;
-					if (sorted.size() >= 2) {
-						fpixel = sorted.get(0).first;
-						bpixel = sorted.get(1).first;
-					} else {
-						fpixel = bpixel = sorted.get(0).first;
-					}
-					
-					for (int xd = x; xd < maxx; xd++) {
-						int newPixel = img.getRGB(xd, y);
-						
-						if (newPixel != fpixel && newPixel != bpixel) {
-							if (fpixel < bpixel) {
-								newPixel = newPixel <= fpixel ? fpixel : bpixel;
-							} else {
-								newPixel = newPixel < bpixel ? fpixel : bpixel;
-							}
-						}
-							
-						imageData.setPixel(xd + xo, y + yo, newPixel);
-					}
-				}
-			}
+			reduceBitmapMode(img, xo, yo);
 
 		} else {
 
@@ -281,7 +232,276 @@ public abstract class ImageDataCanvas extends BitmapVdpCanvas {
 		}
 		
 	}
+
+	/**
+	 * @param img
+	 * @param xo
+	 * @param yo
+	 */
+	protected void reduceBitmapMode(BufferedImage img, int xo, int yo) {
+		Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+		
+		for (int y = 0; y < img.getHeight(); y++) {
+			for (int x = 0; x < img.getWidth(); x += 8) {
+
+				histogram.clear();
+				
+				int maxx = Math.min(x + 8, img.getWidth());
+				
+				for (int xd = x; xd < maxx; xd++) {
+					int pixel = img.getRGB(xd, y);
+					
+					Integer cnt = histogram.get(pixel);
+					if (cnt == null)
+						cnt = 1;
+					else
+						cnt++;
+					histogram.put(pixel, cnt);
+				}
+				
+				// get prominent colors
+				List<Pair<Integer, Integer>> sorted = new ArrayList<Pair<Integer,Integer>>();
+				for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+					sorted.add(new Pair<Integer, Integer>(entry.getKey(), entry.getValue()));
+				}
+				Collections.sort(sorted, new Comparator<Pair<Integer, Integer>>() {
+
+					@Override
+					public int compare(Pair<Integer, Integer> o1,
+							Pair<Integer, Integer> o2) {
+						return o2.second - o1.second;
+					}
+					
+				});
+
+				int fpixel, bpixel;
+				if (sorted.size() >= 2) {
+					fpixel = sorted.get(0).first;
+					bpixel = sorted.get(1).first;
+				} else {
+					fpixel = bpixel = sorted.get(0).first;
+				}
+				
+				for (int xd = x; xd < maxx; xd++) {
+					int newPixel = img.getRGB(xd, y);
+					
+					if (newPixel != fpixel && newPixel != bpixel) {
+						if (fpixel < bpixel) {
+							newPixel = newPixel <= fpixel ? fpixel : bpixel;
+						} else {
+							newPixel = newPixel < bpixel ? fpixel : bpixel;
+						}
+					}
+						
+					imageData.setPixel(xd + xo, y + yo, newPixel);
+				}
+			}
+		}
+	}
 	
+	private int rgbIdx(int r, int g, int b) {
+		return r*64 + g*8 + b;
+	}
+	
+	private int scaleReduce(int rgb, int shift) {
+		int val = (rgb >>> shift) & 0xff;
+		
+		if (val > 255)
+			val = 255;
+		val = ((val >>> 5)) & 0x7;
+		return val;
+		
+	}
+	
+	/*
+	private int scaleReduceBias(int rgb, int shift, boolean high) {
+		int val = (rgb >>> shift) & 0xff;
+		
+		if (high) {
+			val = val * 255 / 0xdf;
+			if (val > 255)
+				val = 255;
+		}
+		else
+			val = val * 0xdf / 0xff;
+		val = ((val >>> 5)) & 0x7;
+		return val;
+		
+	}*/
+	
+	/**
+	 * Update the palette for the optimal distribution of colors.
+	 * 
+	 * The V9938 palette only has 3 bits of precision for each of R,G,B
+	 * so we just reduce each color to 3-3-3 and make a histogram.
+	 * 
+	 * The histogram is sorted by frequency of colors.
+	 * 
+	 * The histogram may show a small number of "important" colors,
+	 * e.g. in a cartoon.  Or it may have a medium number of important
+	 * colors, for art.  Or it may have a large number of
+	 * colors all relatively of the same importance, in a photograph.
+	 * 
+	 * So, rather than taking the 15 most prominent colors, we take
+	 * a sampling of the most important colors covering 25% of the
+	 * image.
+	 * 
+	 * The image will still be dithered without any knowledge of this
+	 * palette alteration, so we need to update the image to physically
+	 * replace the selected colors with their exact RGB values, so 
+	 * dithering will not turn a solid color into a mess.
+	 * 
+	 * @param img
+	 */
+	private void optimizePalette(BufferedImage img) {
+		final float[] hist = new float[512];
+		int[] rgbs = new int[img.getWidth()];
+		//double[] xImp = new double[img.getWidth()];
+		for (int y = 0; y < img.getHeight(); y++) {
+			//double yImportance = Math.sin(y * Math.PI / img.getHeight());
+			img.getRGB(0, y, img.getWidth(), 1, rgbs, 0, rgbs.length);
+			for (int i = 0; i < rgbs.length; i++) {
+				/*double xImportance;
+				if (y == 0) {
+					xImp[i] = Math.sin(i * Math.PI / rgbs.length);
+				}
+				xImportance = xImp[i];
+				
+				double importance = xImportance + yImportance;
+				//double importance = 1; // + Math.sqrt(xImportance * yImportance);
+				*/
+				
+				int rgb = rgbs[i];
+				int r = scaleReduce(rgb, 16);
+				int g = scaleReduce(rgb, 8);
+				int b = scaleReduce(rgb, 0);
+				hist[rgbIdx(r,g,b)] += 0.5f;
+				
+				/*
+				if (false && importance >= 0.25) {
+					// due to loss of color in the palette, see
+					// if any of the colors are hiding in nearby ones,
+					// so dithering will introduce these colors 
+					int hr = scaleReduceBias(rgb, 16, true);
+					int hg = scaleReduceBias(rgb, 8, true);
+					int hb = scaleReduceBias(rgb, 0, true);
+					//if (hr != r || hg != g || hb != b)
+						hist[rgbIdx(hr,hg,hb)] += 0.125f * importance;
+					
+					int lr = scaleReduceBias(rgb, 16, false);
+					int lg = scaleReduceBias(rgb, 8, false);
+					int lb = scaleReduceBias(rgb, 0, false);
+					//if (lr != r || lg != g || lb != b)
+						hist[rgbIdx(lr,lg,lb)] += 0.375 * importance;
+				}
+				*/
+			}
+		}
+		List<Integer> indices = new ArrayList<Integer>(512);
+		for (int i = 0; i < 512; i++)
+			indices.add(i);
+		
+		Collections.sort(indices, new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return (int) Math.signum(hist[o2] - hist[o1]);
+			}
+		});
+		
+		// take sampling of most prominent colors
+		float goal = 0;
+		for (float h : hist)
+			goal += h;
+		
+		//goal *= 0.75f;
+		
+		int interestingColors = 0;
+		while (interestingColors < 512) {
+			int idx = indices.get(interestingColors);
+			if (hist[idx] == 0)
+				break;
+			goal -= hist[idx];
+			if (goal < 0 && interestingColors >= 15)
+				break;
+			interestingColors++;
+		}
+
+		int usedColors = Math.min(15, interestingColors);
+		
+		boolean highColors = interestingColors >= 128;
+		
+		System.out.println("interestingColors="+interestingColors+"; usedColors="+usedColors+"; highColor="+highColors);
+		
+		/* select colors, biasing toward the most prominent:
+		 
+		 index = exp(i * K) - 1
+		 
+		 interestingColors = exp(usedColors * K) - 1
+		 interestingColors + 1 = exp(usedColors * K)
+		 ln(interestingColors + 1) = usedColors * K
+		 ln(interestingColors + 1) / usedColors = K
+		
+		*/
+		
+		double K = Math.log(interestingColors - usedColors / 2) / usedColors;
+		
+		int prev = -1;
+		for (int i = 0; i < usedColors; i++) {
+			int c = i + 1;
+			int index;
+			if (interestingColors == usedColors || i < usedColors / 2) {
+				index = i;
+			} else {
+				//int idx = indices.get(i * interestingColors / usedColors);
+				//int index = interestingColors - (int) (interestingColors * Math.log10(1 + i * K)) - 1;
+				index = (int) Math.expm1(i * K) + usedColors / 2;
+				if (index == prev)
+					index++;
+			}
+			System.out.println("index="+index);
+			prev = index;
+			int idx = indices.get(index);
+			int r = (idx>>6) & 0x7;
+			int g = (idx>>3) & 0x7;
+			int b = (idx>>0) & 0x7;
+			setGRB333(c, g, r, b);
+			
+			if ((interestingColors == usedColors || i < (highColors ? 4 : 8))) {
+				// ensure there will be an exact match so no dithering occurs on the primary ones
+				// for this color
+				byte[] rgb = getRGB(c);
+				int newRGB = ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | ((rgb[2] & 0xff));
+				replaceColor(img, rgbs, r, g, b, newRGB);
+			}
+		}
+	}
+
+	/**
+	 * @param img
+	 * @param rgbs 
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param rgb
+	 */
+	private void replaceColor(BufferedImage img, int[] rgbs, int theR, int theG, int theB, int newRGB) {
+		for (int y = 0; y < img.getHeight(); y++) {
+			img.getRGB(0, y, img.getWidth(), 1, rgbs, 0, rgbs.length);
+			for (int i = 0; i < rgbs.length; i++) {
+				int rgb = rgbs[i];
+				int r = scaleReduce(rgb, 16);
+				int g = scaleReduce(rgb, 8);
+				int b = scaleReduce(rgb, 0);
+				if (r == theR && g == theG && b == theB) {
+					rgbs[i] = newRGB;
+				}
+			}
+			img.setRGB(0, y, img.getWidth(), 1, rgbs, 0, rgbs.length);
+		}
+
+	}
+
 	public byte getPixel(int x, int y) {
 		if (paletteMappingDirty) {
 			updatePaletteMapping();
