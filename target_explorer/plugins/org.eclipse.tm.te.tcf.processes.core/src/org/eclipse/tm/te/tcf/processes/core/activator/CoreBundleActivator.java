8d38c15276e84d20d79d42d03d180966fdf5d80c
Uwe Stieber <uwe.stieber@windriver.com>
2011-10-07 07:20:08 +0200

    Target Explorer: Added processes service support

---
+++ target_explorer/plugins/org.eclipse.tm.te.tcf.processes.core/src/org/eclipse/tm/te/tcf/processes/core/activator/CoreBundleActivator.java
@@ -0,0 +1,30 @@
+package org.eclipse.tm.te.tcf.processes.core.activator;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class CoreBundleActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		CoreBundleActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		CoreBundleActivator.context = null;
+	}
+
+}
