8fcf384ff08b81f05dcfb8b0909520ca8f7a1d25
Uwe Stieber <uwe.stieber@windriver.com>
2011-11-15 12:13:48 +0100

    Target Explorer: Set default TCF terminal type to "ansi"

--- target_explorer/plugins/org.eclipse.tm.te.tcf.terminals.core/src/org/eclipse/tm/te/tcf/terminals/core/launcher/TerminalsLauncher.java
+++ target_explorer/plugins/org.eclipse.tm.te.tcf.terminals.core/src/org/eclipse/tm/te/tcf/terminals/core/launcher/TerminalsLauncher.java
@@ -593,9 +593,9 @@ public class TerminalsLauncher extends PlatformObject implements ITerminalsLaunc
 
 		// Get the terminal attributes
 
-		// Terminal Type: Default to "vt100" if not explicitly specified
+		// Terminal Type: Default to "ansi" if not explicitly specified
 		String type = properties.getStringProperty(ITerminalsLauncher.PROP_TERMINAL_TYPE);
-		if (type == null || "".equals(type.trim())) type = "vt100"; //$NON-NLS-1$ //$NON-NLS-2$
+		if (type == null || "".equals(type.trim())) type = "ansi"; //$NON-NLS-1$ //$NON-NLS-2$
 
 		// Terminal Encoding: Default to "null" if not explicitly specified
 		String encoding = properties.getStringProperty(ITerminalsLauncher.PROP_TERMINAL_ENCODING);
