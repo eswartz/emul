53bac691ab1944c404c870bd728c4327a97d10c8
Ed Swartz <ejs@juncture>
2011-11-20 15:47:59 -0600

    Fixes for Windows -- in progress

--- v9t9/v9t9-java/v9t9-java/data/release/v9t9.sh
+++ v9t9/v9t9-java/v9t9-java/data/release/v9t9.sh
@@ -3,18 +3,30 @@
 NORMSCRIPT=$(echo $0 | sed 's/\\/\//g')
 BASEDIR=$(dirname "$NORMSCRIPT")
 
-PLAT=win32
-WS=win32
 ARCH=$(uname -m)
+if [ "$ARCH" = "i686" ]; then
+	ARCH=x86
+fi
 
+JAVA=java
 OS=$(uname)
 if [ "$OS" = "Linux" ]; then
 	OS=linux
 	WS=gtk
+else
+	# TODO: distinguish MacOS X...
+	OS=win32
+	WS=win32
+	
+	if [ "$PROCESSOR_ARCHITEW6432" = "AMD64" ] ; then
+		ARCH=x86_64
+		JAVA="C:/program files/java/jre6/bin/java"
+	fi
 fi
 
 SWT=org.eclipse.swt.${WS}.${OS}.${ARCH}_3.6.2.v3659c.jar
 echo $SWT
+echo $JAVA
 
 cd "$BASEDIR"
-java -Djava.library.path=lwjgl/$OS:$SWT -Xmx256M -jar v9t9j.jar --swtgl
+"$JAVA" -Djava.library.path=lwjgl/$OS -cp $SWT -Xmx256M -jar v9t9j.jar --swtgl
