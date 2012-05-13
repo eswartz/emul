4c2e9bead9aa7e50767fd90bf78476eae8861f6c
Ed Swartz <eswartz@austin.rr.com>
2012-05-13 10:09:17 -0500

    Revamp deployment instructions and scripts

--- v9t9/v9t9-java/v9t9-gui/data/release/test-httpd.py
+++ v9t9/v9t9-java/v9t9-gui/data/release/test-httpd.py
@@ -8,6 +8,10 @@ from twisted.web.resource import Resource
 class MyResource(Resource):
     def render_GET(self, request):
         return "<html>Hello, world!</html>"
+
+if "github" in "$v9t9.host":
+	print "NOTE: the last build is targeting $v9t9.host (-Drelease=1),\nso this script will not work"
+	sys.exit(1)
     
 root = MyResource()
 v9t9 = static.File(os.path.split(os.path.realpath(__file__))[0])
@@ -23,7 +27,7 @@ sc = service.IServiceCollection(application)
 i = internet.TCPServer(8080, site)
 i.setServiceParent(sc)
 
-print "Connect to localhost:8080/" + childEl + "/v9t9.html"
+print "Connect to $v9t9.host/" + childEl + "/v9t9.html"
 reactor.listenTCP(8080, server.Site(root))
 reactor.run()
 
