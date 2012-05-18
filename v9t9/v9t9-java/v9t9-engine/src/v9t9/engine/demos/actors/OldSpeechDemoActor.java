920cc92ea51843f21929a363128e9f377608b3b2
Ed Swartz <eswartz@austin.rr.com>
2012-05-17 22:47:23 -0500

    don't have both speech actors save talk rate, or else they lose the real value

--- v9t9/v9t9-java/v9t9-engine/src/v9t9/engine/demos/actors/OldSpeechDemoActor.java
+++ v9t9/v9t9-java/v9t9-engine/src/v9t9/engine/demos/actors/OldSpeechDemoActor.java
@@ -7,15 +7,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import ejs.base.properties.IProperty;
-import ejs.base.properties.IPropertyListener;
-import v9t9.common.demo.IDemoEvent;
-import v9t9.common.demo.IDemoHandler;
-import v9t9.common.demo.IDemoPlayer;
-import v9t9.common.demo.IDemoRecorder;
-import v9t9.common.hardware.ISpeechChip;
+import v9t9.common.demos.IDemoEvent;
+import v9t9.common.demos.IDemoPlayer;
+import v9t9.common.demos.IDemoRecorder;
 import v9t9.common.machine.IMachine;
-import v9t9.common.settings.Settings;
 import v9t9.common.speech.ILPCParameters;
 import v9t9.common.speech.ILPCParametersListener;
 import v9t9.engine.demos.events.OldSpeechEvent;
@@ -34,10 +29,6 @@ public class OldSpeechDemoActor extends BaseDemoActor {
 	private List<ILPCParameters> currentPhraseParamsList;
 	
 	private SpeechDemoConverter converter; 
-	private IProperty demoRate;
-	private IProperty talkRate;
-	private double origTalkRate;
-	private IPropertyListener demoRateListener;
 	private ILPCParametersListener convertParamsListener;
 
 	/* (non-Javadoc)
@@ -126,23 +117,6 @@ public class OldSpeechDemoActor extends BaseDemoActor {
 			}
 		};
 		converter.addEquationListener(convertParamsListener);
-		
-		//speech.reset();
-		
-		// have the talk speed match the demo speed
-		talkRate = Settings.get(player.getMachine(), ISpeechChip.settingTalkSpeed);
-		demoRate = Settings.get(player.getMachine(), IDemoHandler.settingDemoPlaybackRate);
-
-		origTalkRate = talkRate.getDouble();
-		
-		demoRateListener = new IPropertyListener() {
-			
-			@Override
-			public void propertyChanged(IProperty property) {
-				talkRate.setDouble(property.getDouble());
-			}
-		};
-		demoRate.addListenerAndFire(demoRateListener);
 	}
 	
 	/* (non-Javadoc)
@@ -180,9 +154,6 @@ public class OldSpeechDemoActor extends BaseDemoActor {
 		super.cleanupPlayback(player);
 		
 		converter.removeEquationListener(convertParamsListener);
-		
-		demoRate.removeListener(demoRateListener);
-		talkRate.setDouble(origTalkRate);
 		
 		//speech.reset();
 	}
