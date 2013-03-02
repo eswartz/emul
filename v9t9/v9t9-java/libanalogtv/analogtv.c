0b648bad81e22e10d5f22458d7e9b0c7eb6e28cd
Ed Swartz <eswartz@austin.rr.com>
2013-03-01 21:16:55 -0600

    Add copyright/GPL headers

--- v9t9/v9t9-java/libanalogtv/analogtv.c
+++ v9t9/v9t9-java/libanalogtv/analogtv.c
@@ -1,14 +1,23 @@
-/* analogtv, Copyright (c) 2003, 2004 Trevor Blackwell <tlb@tlb.org>
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation.  No representations are made about the suitability of this
- * software for any purpose.  It is provided "as is" without express or
- * implied warranty.
- */
+/*
+  analogtv.c
 
+  (c) 2010-2011 Edward Swartz
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+ */
 /*
  * Modified by Ed Swartz for use in V9t9j
  */
