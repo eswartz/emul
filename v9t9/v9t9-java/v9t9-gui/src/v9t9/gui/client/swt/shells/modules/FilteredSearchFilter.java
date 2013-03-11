5f86763b8073f654e4ca005d74fac310276c4e09
Ed Swartz <eswartz@austin.rr.com>
2013-03-11 16:56:18 -0500

    Update dates on new files

--- v9t9/v9t9-java/v9t9-gui/src/v9t9/gui/client/swt/shells/modules/FilteredSearchFilter.java
+++ v9t9/v9t9-java/v9t9-gui/src/v9t9/gui/client/swt/shells/modules/FilteredSearchFilter.java
@@ -1,5 +1,12 @@
-/**
- * 
+/*
+  FilteredSearchFilter.java
+
+  (c) 2013 Ed Swartz
+
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the Eclipse Public License v1.0
+  which accompanies this distribution, and is available at
+  http://www.eclipse.org/legal/epl-v10.html
  */
 package v9t9.gui.client.swt.shells.modules;
 
