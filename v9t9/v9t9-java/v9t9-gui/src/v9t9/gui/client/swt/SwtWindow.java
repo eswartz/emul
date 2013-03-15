750a2987079ece85d50db39774c9651d1e344fe9
Ed Swartz <eswartz@austin.rr.com>
2013-03-15 17:00:02 -0500

    Support context menu via Ctrl-Button1 on Cocoa

--- v9t9/v9t9-java/v9t9-gui/src/v9t9/gui/client/swt/SwtWindow.java
+++ v9t9/v9t9-java/v9t9-gui/src/v9t9/gui/client/swt/SwtWindow.java
@@ -23,8 +23,6 @@ import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
-import org.eclipse.swt.dnd.DND;
-import org.eclipse.swt.dnd.DropTarget;
 import org.eclipse.swt.dnd.RTFTransfer;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.events.ControlAdapter;
@@ -333,7 +331,7 @@ public class SwtWindow extends BaseEmulatorWindow {
 			
 			public void mouseDown(final MouseEvent e) {
 				//System.out.println("Mouse detected " + e);
-				if (!SWT.getPlatform().equals("win32") && e.button == 3) {
+				if (!SWT.getPlatform().equals("win32") && isContextClick(e)) {
 					showAppMenu(e);
 				}
 			}
@@ -343,7 +341,7 @@ public class SwtWindow extends BaseEmulatorWindow {
 					handleClickOutsideToolWindow(videoControl.toDisplay(e.x, e.y));
 					focusRestorer.restoreFocus();
 				}
-				if (SWT.getPlatform().equals("win32") && e.button == 3) {
+				if (SWT.getPlatform().equals("win32") && isContextClick(e)) {
 					showAppMenu(e);
 				}
 			}
@@ -458,6 +456,20 @@ public class SwtWindow extends BaseEmulatorWindow {
 //		});
 	}
 	
+	/**
+	 * @param e
+	 * @return
+	 */
+	protected boolean isContextClick(MouseEvent e) {
+		if (e.button == 3)
+			return true;
+		if (SWT.getPlatform().equals("cocoa") 
+				&& e.button == 1 
+				&& (e.stateMask & SWT.CONTROL) != 0)
+			return true;
+		return false;
+	}
+
 	protected void setFullscreen(boolean fullScreen) {
 		shell.setFullScreen(fullScreen);
 		buttonBar.getButtonBar().setRetractable(true);
