a2c356e5418d0419cba2b3c1f71ba6fd5d1acfe7
Ed Swartz <eswartz@austin.rr.com>
2013-04-01 18:07:43 -0500

    Update copyrights

--- v9t9/v9t9-java/v9t9-machines/tests/v9t9/machine/common/tests/TestMemoryRanges.java
+++ v9t9/v9t9-java/v9t9-machines/tests/v9t9/machine/common/tests/TestMemoryRanges.java
@@ -10,15 +10,15 @@
  */
 package v9t9.machine.common.tests;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import v9t9.common.asm.MemoryRange;
 import v9t9.common.asm.MemoryRanges;
 
-public class TestMemoryRanges extends TestCase {
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
+public class TestMemoryRanges {
+	@Test
     public void testRanges() {
     	MemoryRanges ranges = new MemoryRanges();
         MemoryRange range;
