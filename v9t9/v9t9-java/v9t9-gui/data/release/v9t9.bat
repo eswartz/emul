28b47b90eb3c13637c2200336d9b086694c69c1c
Ed Swartz <eswartz@austin.rr.com>
2013-09-01 08:16:16 -0500

    Fix v9t9.bat for Win 7

--- v9t9/v9t9-java/v9t9-gui/data/release/v9t9.bat
+++ v9t9/v9t9-java/v9t9-gui/data/release/v9t9.bat
@@ -4,4 +4,15 @@ set VMARGS=-Xmx256M -Dlog4j.configuration=jar:file:%~dp0%/v9t9j.jar!/log4j.prope
 
 rem set VMARGS=%VMARGS% -Dlog4j.configuration=jar:file:%~dp0%/v9t9j.jar!/debug.properties
 
-java %VMARGS% -jar %~dp0%\v9t9j.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -version
+if ERRORLEVEL 1 (
+	@echo Java not installed.  Please install it and try again.
+	pause
+	start http://java.com
+) else (
+	rem apparently Win7 does not like this format anymore... 
+	rem java %VMARGS% -jar %~dp0%\v9t9j.jar %*
+	
+	java %VMARGS% -jar %~dp0%\v9t9j.jar
+	pause
+)
