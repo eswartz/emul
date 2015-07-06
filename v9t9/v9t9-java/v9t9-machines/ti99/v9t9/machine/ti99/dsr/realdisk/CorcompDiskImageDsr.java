5c73a1baacb2273b7c1aea12f097fc7e6185fc1f
Ed Swartz <ed.swartz.258@gmail.com>
2015-07-06 09:36:56 -0500

    Update copyrights/licenses

--- v9t9/v9t9-java/v9t9-machines/ti99/v9t9/machine/ti99/dsr/realdisk/CorcompDiskImageDsr.java
+++ v9t9/v9t9-java/v9t9-machines/ti99/v9t9/machine/ti99/dsr/realdisk/CorcompDiskImageDsr.java
@@ -1,3 +1,13 @@
+/*
+  CorcompDiskImageDsr.java
+
+  (c) 2013-2015 Ed Swartz
+
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the Eclipse Public License v1.0
+  which accompanies this distribution, and is available at
+  http://www.eclipse.org/legal/epl-v10.html
+ */
 package v9t9.machine.ti99.dsr.realdisk;
 
 import java.io.IOException;
