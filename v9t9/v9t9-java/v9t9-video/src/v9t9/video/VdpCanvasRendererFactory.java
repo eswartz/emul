0e6f1b54e9c5f23ce0910fbb28a27e7c11e1afef
Ed Swartz <ed.swartz.258@gmail.com>
2015-07-11 11:33:33 -0500

    Turn off scanline renderer for now

--- v9t9/v9t9-java/v9t9-video/src/v9t9/video/VdpCanvasRendererFactory.java
+++ v9t9/v9t9-java/v9t9-video/src/v9t9/video/VdpCanvasRendererFactory.java
@@ -15,9 +15,9 @@ import v9t9.common.client.IVideoRenderer;
 import v9t9.common.hardware.IVdpTMS9918A;
 import v9t9.common.hardware.IVdpV9938;
 import v9t9.common.video.IVdpCanvasRenderer;
+import v9t9.video.tms9918a.VdpTMS9918ACanvasBlockRenderer;
 import v9t9.video.tms9918a.VdpTMS9918ACanvasRowRenderer;
 import v9t9.video.v9938.VdpV9938CanvasBlockRenderer;
-import v9t9.video.v9938.VdpV9938CanvasRowRenderer;
 
 /**
  * @author ejs
@@ -28,11 +28,11 @@ public class VdpCanvasRendererFactory {
 
 	public static IVdpCanvasRenderer createCanvasRenderer(ISettingsHandler settings, IVideoRenderer video) {
 		if (video.getVdpHandler() instanceof IVdpV9938)
-//			return new VdpV9938CanvasBlockRenderer(settings, video);
-			return new VdpV9938CanvasRowRenderer(settings, video);
+			return new VdpV9938CanvasBlockRenderer(settings, video);
+//			return new VdpV9938CanvasRowRenderer(settings, video);
 		else if (video.getVdpHandler() instanceof IVdpTMS9918A)
-//			return new VdpTMS9918ACanvasBlockRenderer(settings, video);
-			return new VdpTMS9918ACanvasRowRenderer(settings, video);
+			return new VdpTMS9918ACanvasBlockRenderer(settings, video);
+//			return new VdpTMS9918ACanvasRowRenderer(settings, video);
 		return null;
 	}
 	
